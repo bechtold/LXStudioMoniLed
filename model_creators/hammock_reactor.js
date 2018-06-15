707a28dd9fcbf7733dca6500bbf2b23bf78de978
Oskar Bechtold <bechtold@brightsolutions.de>
2018-06-16 01:04:55 +0200

    Added polygon calculation to model creator.

--- model_creators/hammock_reactor.js
+++ model_creators/hammock_reactor.js
@@ -1,13 +1,58 @@
 var CSV = require('csv-string');
 
+/*export interface polygonConfig {
+  n: number; // number of vertices = number of sides
+  r: number; // radius (circumscribed circle)
+
+  // could be optional attributes, but need default values TODO: find out how to create default values
+  a: number; // counterclockwise rotation angle in radians
+  cx: number; // center x (offset)
+  cy: number; // center y (offset)
+  // round: number; // number of decimal places to keep (0-20)
+}*/
+
+/*
+ * inspiration: http://math.stackexchange.com/questions/1123175
+ * and: https://stackoverflow.com/questions/3436453/calculate-coordinates-of-a-regular-polygons-vertices
+ * and: https://gist.github.com/jonthesquirrel/e2807811d58a6627ded4
+ * usage: calcPolygon({n: 3, r: 50, a: 0, cx: 0, cy: 0, round: 0})
+*/
+function calcPolygon(input) {
+  let polygon = [];
+  for (let i = 1; i <= input.n; i++) {
+    polygon.push(
+      // ((input.r * Math.cos(input.a + 2 * i * Math.PI / input.n)) + input.cx).toFixed(input.round),
+      // ((input.r * Math.sin(input.a + 2 * i * Math.PI / input.n)) + input.cy).toFixed(input.round)
+      [
+        ((input.r * Math.cos(input.a + 2 * i * Math.PI / input.n)) + input.cx),
+        ((input.r * Math.sin(input.a + 2 * i * Math.PI / input.n)) + input.cy)
+      ]
+    )
+  }
+  return polygon;
+}
 
 // create vertices with https://www.mathopenref.com/coordpolycalc.html
 
 // with String
-const vertices_top = CSV.parse('11591,-31068485,-8485\n3106,-11591\n-3106,-11591\n-8485,-8485\n-11591,-3106\n-11591,3106\n-8485,8485\n-3106,11591\n3106,11591\n8485,8485\n11591,3106');
-const vertices_bottom = CSV.parse('2588,-9659\n-2588,-9659\n-7071,-7071\n-9659,-2588\n-9659,2588\n-7071,7071\n-2588,9659\n2588,9659\n7071,7071\n9659,2588\n9659,-2588\n7071,-7071');
+//const vertices_top = CSV.parse('11591,-3106\n8485,-8485\n3106,-11591\n-3106,-11591\n-8485,-8485\n-11591,-3106\n-11591,3106\n-8485,8485\n-3106,11591\n3106,11591\n8485,8485\n11591,3106');
+//const vertices_bottom = CSV.parse('2588,-9659\n-2588,-9659\n-7071,-7071\n-9659,-2588\n-9659,2588\n-7071,7071\n-2588,9659\n2588,9659\n7071,7071\n9659,2588\n9659,-2588\n7071,-7071');
 // console.log(vertices_top);
 // console.log(vertices_bottom);
+let vertices_top = calcPolygon({
+  n: 12,
+  r: 12000,
+  a: 0,
+  cx: 0,
+  cy: 0
+});
+let vertices_bottom = calcPolygon({
+  n: 12,
+  r: 11000,
+  a: -0.3,
+  cx: 0,
+  cy: 0
+});
 
 var ring_top = [];
 for(i=0; i<12;i++){
@@ -85,7 +130,8 @@ var reactor = {
 }
 
 var json = JSON.stringify(reactor);
-console.log(json);
 
 var fs = require('fs');
-fs.writeFile('hammock_reactor.json', json, 'utf8', function(){});
+fs.writeFile('./hammock_reactor.json', json, 'utf8', function(){
+  
+});
