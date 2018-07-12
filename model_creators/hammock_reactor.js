cabe926c9d065369aa6549ebd22b483c38504bd5
Oskar Bechtold <bechtoldster@gmail.com>
2018-07-12 02:37:12 +0200

    Added tower mapping.

--- model_creators/hammock_reactor.js
+++ model_creators/hammock_reactor.js
@@ -153,24 +153,25 @@ for(i=0; i<12;i++){
 }
 
 var tower = [];
-// for(i=0; i<6;i++){
-//   var strip = 				{
-//             "name"    : "line_"+i,
-//   					"leds"    : 90,
-//   					"artnet"	: { "ip":ip, "universe": 0, "address": 0},
-//   					"start"	  : { "x": tower_top[i][0],	  "y": 3000, "z":tower_top[i][1]},
-//   					"end"     : { "x": tower_bottom[i][0], "y": 0,    "z": tower_bottom[i][1] }
-//   				};
-//   tower.push(strip);
-//   var strip = 				{
-//             "name"    : "line_"+i+"_2",
-//   					"leds"    : 90,
-//   					"artnet"	: { "ip":ip, "universe": 0, "address": 0},
-//   					"start"	  : { "x": tower_top_1[i][0],	  "y": 3000, "z":tower_top_1[i][1]},
-//   					"end"     : { "x": tower_bottom_1[i][0], "y": 0,    "z": tower_bottom_1[i][1] }
-//   				}
-//   tower.push(strip);
-// }
+for(i=0; i<6;i++){
+  var universe_offset = 52;
+  var strip = 				{
+            "name"    : "line_"+i,
+  					"leds"    : 90,
+  					"artnet"	: { "ip":ip, "universe": universe_offset + i*2, "offset": 0, "reverse": false},
+            "start"     : { "x": tower_bottom[i][0], "y": 0,    "z": tower_bottom[i][1] },
+  					"end"	  : { "x": tower_top[i][0],	  "y": 3000, "z":tower_top[i][1]}
+  				};
+  tower.push(strip);
+  var strip = 				{
+            "name"    : "line_"+i+"_2",
+  					"leds"    : 90,
+  					"artnet"	: { "ip":ip, "universe": universe_offset + i*2, "offset": 90, "reverse": false},
+  					"start"	  : { "x": tower_top_1[i][0],	  "y": 3000, "z":tower_top_1[i][1]},
+  					"end"     : { "x": tower_bottom_1[i][0], "y": 0,    "z": tower_bottom_1[i][1] }
+  				}
+  tower.push(strip);
+}
 
 var reactor = {
 	"elements": [
