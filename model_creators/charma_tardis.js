1244d462eba3d60fea0eb748dc3733d3a7afbfaf
Oskar Bechtold <bechtoldster@gmail.com>
2018-09-04 02:45:09 +0200

    Cleaned up old leftovers.

--- model_creators/charma_tardis.js
+++ model_creators/charma_tardis.js
@@ -112,20 +112,13 @@ function create_pillar(universe, universes_per_output, universe_offset){
     }
 
     var rev = !!(i%2);
-
     var universe_shift = Math.floor( (i*30) / 170 );
     var offset_shifted = (i*30) % 170;
-    // console.log("jo");
-    // console.log(i*30);
-    // console.log(universes_per_output*170);
-    // console.log(universe_shift);
-    // console.log(offset_shift);
 
     var strip = {
       "name"    : "pillar_strip_"+i,
       "leds"    : 30,
       "artnet"	: { "ip":ip, "universe": universe + universe_shift, "offset": offset_shifted, reverse: rev},
-      // "artnet"	: { "ip":ip, "universe": universe, "offset": i*30, reverse: rev},
       "start"	  : { "x": x, "y": 4000, "z": z},
       "end"     : { "x": x, "y": 3000, "z": z}
     }
@@ -168,145 +161,15 @@ function pillars(universes_per_output, universe_offset) {
   }
   return pillars;
 }
-// var ring_top = [];
-// for(i=0; i<12;i++){ // i=0, i<12
-//   var rev = !(i%2)
-//   var strip =         {
-//             "name"    : "ring_top_"+i,
-//   					"leds"    : 90,
-//   					"artnet"	: { "ip":ip, "universe": i*2, "offset": 0, reverse: rev},
-//   					"start"	  : { "x": vertices_top[i][0],	  "y": 3000, "z":vertices_top[i][1]},
-//   					"end"     : { "x": vertices_top[(i+1)%vertices_top.length][0], "y":3000,    "z": vertices_top[(i+1)%vertices_top.length][1] }
-//   				}
-//   ring_top.push(strip);
-// }
-
-// var ring_bottom = [];
-// for(i=0; i<12;i++){
-//   var strip = 				{
-//             "name"    : "ring_bottom_"+i,
-//   					"leds"    : 90,
-//   					"artnet"	: { "ip":ip, "universe": 0, "offset": 0},
-//   					"start"	  : { "x": vertices_bottom[i][0],	  "y": 0, "z":vertices_bottom[i][1]},
-//   					"end"     : { "x": vertices_bottom[(i+1)%vertices_bottom.length][0], "y": 0,    "z": vertices_bottom[(i+1)%vertices_bottom.length][1] }
-//   				}
-//   ring_bottom.push(strip);
-// }
-
-// var triangles = [];
-// for(i=0; i<12;i++){ // i=1; i<12
-//   var rev = !(i%2);
-//
-//   if(rev) {
-//     var strip = 				{
-//               "name"    : "triangle_"+i,
-//     					"leds"    : 90,
-//     					"artnet"	: { "ip":ip, "universe": i*2, "offset": 90, "reverse": false},
-//     					"start"	  : { "x": vertices_top[i][0],	  "y": 3000, "z":vertices_top[i][1]},
-//     					"end"     : { "x": vertices_bottom[(i+1)%vertices_top.length][0], "y": 0,    "z": vertices_bottom[(i+1)%vertices_top.length][1] }
-//     				};
-//     triangles.push(strip);
-//     var strip = 				{
-//               "name"    : "triangle_"+i+"_2",
-//     					"leds"    : 90,
-//     					"artnet"	: { "ip":ip, "universe": i*2 + 1, "offset": 10, "reverse": false},
-//     					"start"   : { "x": vertices_bottom[(i+1)%vertices_top.length][0], "y": 0,    "z": vertices_bottom[(i+1)%vertices_top.length][1] },
-//               "end"	    : { "x": vertices_top[(i+1)%vertices_top.length][0],	  "y": 3000, "z":vertices_top[(i+1)%vertices_top.length][1]}
-//     				}
-//     triangles.push(strip);
-//   }else {
-//
-//     var strip = 				{
-//               "name"    : "triangle_"+i,
-//     					"leds"    : 90,
-//     					"artnet"	: { "ip":ip, "universe": i*2 + 1, "offset": 10, "reverse": true},
-//               "start"	  : { "x": vertices_top[i][0],	  "y": 3000, "z":vertices_top[i][1]},
-//               "end"     : { "x": vertices_bottom[(i+1)%vertices_top.length][0], "y": 0,    "z": vertices_bottom[(i+1)%vertices_top.length][1] }
-//     				};
-//     triangles.push(strip);
-//
-//     // Bug with reverse overflow!
-//     var strip = 				{
-//               "name"    : "triangle_"+i+"_2",
-//     					"leds"    : 90,
-//     					"artnet"	: { "ip":ip, "universe": i*2, "offset": 90, "reverse": true},
-//     					"start"   : { "x": vertices_bottom[(i+1)%vertices_top.length][0], "y": 0,    "z": vertices_bottom[(i+1)%vertices_top.length][1] },
-//               "end"	    : { "x": vertices_top[(i+1)%vertices_top.length][0],	  "y": 3000, "z":vertices_top[(i+1)%vertices_top.length][1]}
-//     				}
-//     triangles.push(strip);
-//
-//   }
-// }
-//
-// var tower = [];
-// for(i=0; i<6;i++){
-//   var universe_offset = 52;
-//   var strip = 				{
-//             "name"    : "line_"+i,
-//   					"leds"    : 90,
-//   					"artnet"	: { "ip":ip, "universe": universe_offset + i*2, "offset": 0, "reverse": false},
-//             "start"     : { "x": tower_bottom[i][0], "y": 0,    "z": tower_bottom[i][1] },
-//   					"end"	  : { "x": tower_top[i][0],	  "y": 3000, "z":tower_top[i][1]}
-//   				};
-//   tower.push(strip);
-//   var strip = 				{
-//             "name"    : "line_"+i+"_2",
-//   					"leds"    : 90,
-//   					"artnet"	: { "ip":ip, "universe": universe_offset + i*2, "offset": 90, "reverse": false},
-//   					"start"	  : { "x": tower_top_1[i][0],	  "y": 3000, "z":tower_top_1[i][1]},
-//   					"end"     : { "x": tower_bottom_1[i][0], "y": 0,    "z": tower_bottom_1[i][1] }
-//   				}
-//   tower.push(strip);
-// }
 
 var tardis = {
   "name": "tardis",
-	"elements": [
-		// {
-		// 	"name": "Ring Bottom",
-		// 	"x":0,
-		// 	"y":0,
-		// 	"strips": ring_bottom
-		// },
-		// {
-		// 	"name": "Ring Top",
-		// 	"x":0,
-		// 	"y":0,
-		// 	"strips": ring_top
-		// },
-    // {
-		// 	"name": "Triangles",
-		// 	"x":0,
-		// 	"y":0,
-		// 	"strips": triangles
-    // },
-
-
-
-    // {
-		// 	"name": "Side Panel",
-		// 	"x": 4500,
-		// 	"y": 0,
-    //   "z": 0,
-    //   "group": "right panels",
-		// 	"strips": side_panel
-    // },
-    // {
-		// 	"name": "Side Panel 2",
-		// 	"x": 4500,
-		// 	"y": 0,
-    //   "z": 1500,
-    //   "group": "right panels",
-		// 	"strips": side_panel
-    // }
-
-
-  ]
+	"elements": []
 }
-// tardis.elements = tardis.elements.concat(side_panels_right(3, 0));
-// tardis.elements = tardis.elements.concat(side_panels_left(3, 0));
+tardis.elements = tardis.elements.concat(side_panels_right(3, 0));
+tardis.elements = tardis.elements.concat(side_panels_left(3, 0));
 tardis.elements = tardis.elements.concat(back_panels(3, 0));
-// tardis.elements = tardis.elements.concat(pillars(3, 0));
+tardis.elements = tardis.elements.concat(pillars(3, 0));
 
 var json = JSON.stringify(tardis, null, ' ');
 
